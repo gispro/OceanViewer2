5355dba4d962c7f8e66071cc73924b8d60d1909a
dtyugaev <dtyugaev@gispro.ru>
2011-05-24 18:47:23 +0400

    Translate to Russian

---
+++ app/static/script/app/locale/ru.js
@@ -0,0 +1,36 @@
+﻿/**
+ * Copyright (c) 2009-2010 The Open Planning Project
+ */
+
+GeoExt.Lang.add("ru", {
+    "GeoExplorer.prototype": {
+        zoomSliderText: "<div>Уровень: {zoom}</div><div>Масштаб: 1:{scale}</div>",
+        loadConfigErrorText: "Ошибка чтения сохраненной конфигурации: <br />",
+        loadConfigErrorDefaultText: "Ошибка сервера.",
+        xhrTroubleText: "Ошибка связи: Статус ",
+        layersText: "Слои",
+        titleText: "Заголовок",
+        zoomLevelText: "Уровень",
+        saveErrorText: "Ошибка сохранения: ",
+        bookmarkText: "URL закладки",
+        permakinkText: 'Постоянная ссылка',
+        appInfoText: "GeoExplorer",
+        aboutText: "О GeoExplorer",
+        mapInfoText: "Описание карты",
+        descriptionText: "Описание",
+        contactText: "Контакты",
+        aboutThisMapText: "Об этой карте"
+    },
+    "GeoExplorer.Composer.prototype": {
+        exportMapText: "Экспортировать карту",
+        saveMapText: "Сохранить карту",
+        toolsTitle: "Выберете инструмент на панель:",
+        previewText: "Предпросмотр",
+        backText: "Назад",
+        nextText: "Далее",
+        loginText: "Вход",
+        loginErrorText: "Неверный пароль или имя пользователя.",
+        userFieldText: "Пользователь",
+        passwordFieldText: "Пароль"
+    }
+});
