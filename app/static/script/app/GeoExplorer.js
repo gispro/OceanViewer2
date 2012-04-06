2dcab17556d7943195a390f86cf6c5ce4915b7a1
Kirill Jakovlev <special-k@li.ru>
2012-04-07 00:38:42 +0400

    panel rename

--- app/static/script/app/GeoExplorer.js
+++ app/static/script/app/GeoExplorer.js
@@ -162,7 +162,6 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
     }, 
 
     loadConfig: function(config) {
-        
         var mapUrl = window.location.hash.substr(1);
         var match = mapUrl.match(/^maps\/(\d+)$/);
         if (match) {
@@ -307,7 +306,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
                 items: [
                   {
                       xtype: 'panel',
-                      title: 'Моя панель',
+                      title: 'Общие',
                       collapsible: true,
                       items: [
                         {
